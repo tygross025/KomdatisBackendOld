e134155a49ba4200f97db8d0484766c34f09a7c9
Ty Gross <31942911+tygross025@users.noreply.github.com>
2023-10-04 15:42:46 +0200

    Added Contract Definition Creation for Buildings

--- src/main/kotlin/com/komdatis/service/BuildingService.kt
+++ src/main/kotlin/com/komdatis/service/BuildingService.kt
@@ -1,9 +1,17 @@
 package com.komdatis.service
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.komdatis.model.Building
 import com.komdatis.dto.BuildingDto
 import com.komdatis.repository.BuildingRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClientException
+import org.springframework.web.client.RestTemplate
 
 @Service
 class BuildingService(
@@ -11,13 +19,30 @@ class BuildingService(
     @Autowired private val buildingConverterService: BuildingConverterService
 ) {
 
+    @Value("\${connector.url}")
+    private lateinit var connectorUrl: String
+    @Value("\${connector.auth}")
+    private lateinit var connectorAuth: String
+
     fun getAllBuildings(): List<BuildingDto> =
         buildingRepository.findAll().map { buildingConverterService.toDto(it) }
 
-    fun createBuilding(buildingDto: BuildingDto): BuildingDto {
+    fun createBuilding(buildingDto: BuildingDto): BuildingDto? {
         val building = buildingConverterService.fromDto(buildingDto)
         building.warmWater.forEach { it.building = building }
         building.warmth.forEach { it.building = building }
+        
+        val restTemplate = RestTemplate()
+
+        val assetRequest = generateAssetRequest(building)
+        val contractDefinitionRequest = generateContractDefinitionRequest(building)
+
+        try {
+            restTemplate.postForObject("$connectorUrl/assets", assetRequest, String::class.java)
+            restTemplate.postForObject("$connectorUrl/contractdefinitions", contractDefinitionRequest, String::class.java)
+        } catch (exception: RestClientException) {
+            return null
+        }
         val savedBuilding = buildingRepository.save(building)
         return buildingConverterService.toDto(savedBuilding)
     }
@@ -50,5 +75,69 @@ class BuildingService(
         } else {
             false
         }
+    }
+
+    /**
+     * generates the request needed to create an asset on the connector.
+     */
+    private fun generateAssetRequest(building: Building): HttpEntity<JsonNode> {
+        val headers = HttpHeaders()
+        headers.add("x-api-key", connectorAuth)
+
+        val jsonString = "{\n" +
+                "            \"@context\": {\n" +
+                "            \"edc\": \"https://w3id.org/edc/v0.0.1/ns/\"\n" +
+                "        },\n" +
+                "            \"edc:asset\": {\n" +
+                "            \"@id\": \"" + building.address + "\",\n" +
+                "            \"edc:properties\": {\n" +
+                "            \"edc:name\": \"" + building.address + "\",\n" +
+                "            \"edc:contenttype\": \"appliaction/json\",\n" +
+                "            \"edc:version\": \"1.0\",\n" +
+                "            \"edc:type\": \"HttpData\"\n" +
+                "        }\n" +
+                "        },\n" +
+                "            \"edc:dataAddress\": {\n" +
+                "            \"edc:type\": \"HttpData\",\n" +
+                "            \"edc:properties\": {\n" +
+                "            \"edc:name\": \"hi\",\n" +
+                "            \"baseUrl\": \"https://baseurl\",\n" + //todo missing baseurl
+                "            \"edc:type\": \"HttpData\"\n" +
+                "        }\n" +
+                "        }\n" +
+                "        }"
+
+        val mapper = ObjectMapper()
+        val node = mapper.readTree(jsonString)
+        return HttpEntity(node, headers)
+    }
+
+    /**
+     * generates the request needed to create a contract definition on the connector.
+     */
+    private fun generateContractDefinitionRequest(building: Building): HttpEntity<JsonNode> {
+        val headers = HttpHeaders()
+        headers.add("x-api-key", connectorAuth)
+
+        //todo change policy used
+        val jsonString = "{\n" +
+                "    \"@context\": {\n" +
+                "      \"edc\": \"https://w3id.org/edc/v0.0.1/ns/\"\n" +
+                "    },\n" +
+                "    \"@id\": \"" + building.address + "\",\n" +
+                "    \"accessPolicyId\": \"eu-restricted-policy\",\n" +
+                "    \"contractPolicyId\": \"eu-restricted-policy\",\n" +
+                "    \"assetsSelector\": [\n" +
+                "      {\n" +
+                "        \"operandLeft\": \"https://w3id.org/edc/v0.0.1/ns/id\",\n" +
+                "        \"operator\": \"=\",\n" +
+                "        \"operandRight\": \"" + building.address + "\"\n" +
+                "      }\n" +
+                "    ]\n" +
+                "}"
+
+        val mapper = ObjectMapper()
+        val node = mapper.readTree(jsonString)
+        return HttpEntity(node, headers)
     }
 }
